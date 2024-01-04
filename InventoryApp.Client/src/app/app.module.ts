c6b96d70d81bc2d121f2ccb3e070bc676fff6e24
Sohel Rana <srana@ael-bd.com>
2024-01-04 18:56:55 +0600

    component added

--- InventoryApp.Client/src/app/app.module.ts
+++ InventoryApp.Client/src/app/app.module.ts
@@ -3,24 +3,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './common/component/header/header.component';
 import { SidebarComponent } from './common/component/sidebar/sidebar.component';
 import { FooterComponent } from './common/component/footer/footer.component';
+import { AppRoutingModule } from './app.routes';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
-    RouterOutlet,
-    HeaderComponent,
-    SidebarComponent,
-    FooterComponent,
+    //HeaderComponent,
+    RouterModule,
+    //SidebarComponent,
+    //FooterComponent,
+    //HomeComponent,
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoute
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
