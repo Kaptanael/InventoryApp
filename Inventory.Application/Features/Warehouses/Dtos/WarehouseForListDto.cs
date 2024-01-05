e1465e6a4469406e723441fe521a76e8529138c0
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-01-05 17:01:31 +0600

    add dtos for ware house

---
+++ Inventory.Application/Features/Warehouses/Dtos/WarehouseForListDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Inventory.Application.Features;
+
+public class WarehouseForListDto
+{
+    public Guid Id { get; set; }
+
+    public Guid BranchId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string StreetAddress { get; set; }
+
+    public string City { get; set; }
+
+    public string Province { get; set; }
+
+    public string Country { get; set; }
+
+    public Guid CreatedBy { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public Guid UpdatedBy { get; set; }
+
+    public DateTime UpdatedDate { get; set; }
+}
---
+++ Inventory.Application/Features/Warehouses/Dtos/WarehouseForUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Inventory.Application.Features;
+
+public class WarehouseForUpdateDto
+{
+    public Guid Id { get; set; }
+
+    public Guid BranchId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string StreetAddress { get; set; }
+
+    public string City { get; set; }
+
+    public string Province { get; set; }
+
+    public string Country { get; set; }
+    
+}
