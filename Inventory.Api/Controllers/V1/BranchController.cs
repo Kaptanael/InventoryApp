3006b6c9e305dbeaf42dedca6644a7c2c6c1a7e8
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-01-09 14:42:38 +0600

    add branch controller

---
+++ Inventory.Api/Controllers/V1/BranchController.cs
@@ -0,0 +1,43 @@
+﻿namespace Inventory.Api.Controllers.V1;
+
+[Route("api/v1/branch")]
+[ApiController]
+public class BranchController : ControllerBase
+{
+    private readonly IBranchService _branchService;
+
+    public BranchController(IBranchService branchService)
+    {
+        _branchService = branchService;
+    }
+
+    [HttpGet("GetAll")]
+    public async Task<ActionResult<List<BranchForListDto>>> GetAllAsync()
+    {
+        return Ok(await _branchService.GetAll());
+    }
+
+    [HttpGet("GetById/{id}")]
+    public async Task<ActionResult<BranchForListDto>> GetByIdAsync(Guid id)
+    {
+        return Ok(await _branchService.GetById(id));
+    }
+
+    [HttpPost("Create")]
+    public async Task<ActionResult> CrteateAsync([FromBody] BranchForCreateDto request)
+    {
+        return Ok(await _branchService.CreateAsync(request));
+    }
+
+    [HttpPut("Update/{id}")]
+    public async Task<ActionResult> UpdateAsync(Guid id, [FromBody] BranchForUpdateDto request)
+    {
+        return Ok(await _branchService.UpdateAsync(id, request));
+    }
+
+    [HttpDelete("Delete/{id}")]
+    public async Task<ActionResult> DeleteAsync(Guid id)
+    {
+        return Ok(await _branchService.DeleteAsync(id));
+    }
+}
