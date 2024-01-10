afb57d37770d94b382558afa729611b15c94a6c0
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-01-10 14:39:25 +0600

    add product type dtos

---
+++ Inventory.Application/Features/ProductTypes/Dtos/ProductTypeForCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Inventory.Application.Features;
+
+public class ProductTypeForCreateDto
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+    
+}
---
+++ Inventory.Application/Features/ProductTypes/Dtos/ProductTypeForListDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Inventory.Application.Features;
+
+public class ProductTypeForListDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public Guid CreatedBy { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public Guid UpdatedBy { get; set; }
+
+    public DateTime UpdatedDate { get; set; }
+    
+}
