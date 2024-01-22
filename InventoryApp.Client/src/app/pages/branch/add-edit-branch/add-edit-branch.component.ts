0509004ffbecf515a12c1022f83e44f2289cc64a
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-01-22 17:37:14 +0600

    add form controls in add edit branch

--- InventoryApp.Client/src/app/pages/branch/add-edit-branch/add-edit-branch.component.ts
+++ InventoryApp.Client/src/app/pages/branch/add-edit-branch/add-edit-branch.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +9,27 @@ import { Router } from '@angular/router';
 })
 export class AddEditBranchComponent {
 
-  constructor(private router: Router) {
+  public formGroup!: FormGroup;
+  public selectedBranch: any = {};
+  public selectedBranchId: number | undefined;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.createFormGroup();
   }
+
+  createFormGroup() {
+    this.formGroup = this.fb.group({
+      name: ['', Validators.compose([Validators.maxLength(50)])],
+      description: ['', Validators.compose([Validators.maxLength(200)])],
+      street: ['', Validators.compose([Validators.maxLength(200)])],
+      city: ['', Validators.compose([Validators.maxLength(50)])],
+      province: ['', Validators.compose([Validators.maxLength(50)])],
+      country: ['', Validators.compose([Validators.maxLength(50)])]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formGroup);
+  }
+
 }
--- InventoryApp.Client/src/app/pages/branch/add-edit-branch/add-edit-branch.module.ts
+++ InventoryApp.Client/src/app/pages/branch/add-edit-branch/add-edit-branch.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AddEditBranchRoutingModule } from './add-edit-branch-routing.module';
 import { AddEditBranchComponent } from './add-edit-branch.component';
@@ -11,7 +12,8 @@ import { AddEditBranchComponent } from './add-edit-branch.component';
   imports: [
     CommonModule,
     AddEditBranchRoutingModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ]
 })
 export class AddEditBranchModule { }
