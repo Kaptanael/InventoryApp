92f4be1436e1a8821308d1011d2d200390f70d89
Sohel Rana <srana@ael-bd.com>
2024-01-24 17:26:16 +0600

    get user list

--- InventoryApp.Client/src/app/app.routes.ts
+++ InventoryApp.Client/src/app/app.routes.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    /* canActivate: [AuthGuard],*/
+    canActivate: [AuthGuard],
     children: [
       {
         path: "branch", loadChildren: () => import('./pages/branch/branch.module').then(m => m.BranchModule)
