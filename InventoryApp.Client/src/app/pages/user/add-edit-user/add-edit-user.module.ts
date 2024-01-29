cb923da1402fa94370d7c7766c06bba0a0dedcb5
Sohel Rana <srana@ael-bd.com>
2024-01-29 19:31:19 +0600

    Add User UI

--- InventoryApp.Client/src/app/pages/user/add-edit-user/add-edit-user.module.ts
+++ InventoryApp.Client/src/app/pages/user/add-edit-user/add-edit-user.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AddEditUserRoutingModule } from './add-edit-user-routing.module';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddEditUserComponent } from './add-edit-user.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AddEditUserComponent],
   imports: [
     CommonModule,
-    AddEditUserRoutingModule
-  ]
+    AddEditUserRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    ToastModule
+  ],
+  providers: [MessageService]
 })
 export class AddEditUserModule { }
