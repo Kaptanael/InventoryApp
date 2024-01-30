a8a103ac73beec45f65caa5e01a2aee1bff8cadc
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-01-30 14:28:09 +0600

    add unit repository

---
+++ Inventory.Application/Contracts/Persistence/IUnitRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory.Application.Repositories;
+
+public interface IUnitRepository
+{
+    Task<Guid> Create(Unit model);
+    Task<bool> Delete(Unit model);
+    Task<List<Unit>> GetAll();
+    Task<Unit> GetById(Guid id);
+    Task<bool> IsExist(string name, Guid? id = null);
+    Task<bool> Update(Unit model);
+}
---
+++ Inventory.Persistence/Repositories/UnitRepository.cs
@@ -0,0 +1,67 @@
+﻿namespace Inventory.Persistence.Repositories;
+
+public class UnitRepository : IUnitRepository
+{
+    private readonly InventoryDbContext _context;
+
+    public UnitRepository(InventoryDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Unit>> GetAll()
+    {
+        var units = await _context.Units.ToListAsync();
+        return units;
+    }
+
+    public async Task<Unit> GetById(Guid id)
+    {
+        var unit = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+        return unit;
+    }
+
+    public async Task<bool> IsExist(string name, Guid? id = null)
+    {
+        if (id == null)
+        {
+            var unit = await _context.Units.FirstOrDefaultAsync(u => u.Name.ToUpper() == name.Trim().ToUpper());
+            if (unit != null)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            var unit = await _context.Units.FirstOrDefaultAsync(u => u.Name.ToUpper() == name.Trim().ToUpper() && u.Id != id);
+            if (unit != null)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public async Task<Guid> Create(Unit model)
+    {
+        await _context.Units.AddAsync(model);
+        await _context.SaveChangesAsync();
+        return model.Id;
+    }
+
+    public async Task<bool> Update(Unit model)
+    {
+        _context.Units.Update(model);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> Delete(Unit model)
+    {
+        _context.Units.Remove(model);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+}
