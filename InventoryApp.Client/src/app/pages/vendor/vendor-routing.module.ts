ec5ae00229c89e6ec6e99965af4237dd985bfc5e
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-02-01 15:54:23 +0600

    add components for vendor list and add edit vendor

--- InventoryApp.Client/src/app/pages/vendor/vendor-routing.module.ts
+++ InventoryApp.Client/src/app/pages/vendor/vendor-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { VendorComponent } from './vendor.component';
+
 
 const routes: Routes = [{
   path: '',
-  component: VendorComponent
+  children:
+    [
+      {
+        path: '',
+        component: VendorComponent
+      },
+      {
+        path: 'add-edit-vendor', loadChildren: () => import('./add-edit-vendor/add-edit-vendor.module').then(m => m.AddEditVendorModule)
+      },
+      {
+        path: 'add-edit-vendor/:id', loadChildren: () => import('./add-edit-vendor/add-edit-vendor.module').then(m => m.AddEditVendorModule)
+      }
+    ]
 }];
-
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- InventoryApp.Client/src/app/pages/vendor/vendor.component.ts
+++ InventoryApp.Client/src/app/pages/vendor/vendor.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { VendorService } from './vendor.service';
+import { Component, OnInit } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VendorService } from '../../_services/vendor.service';
+
+
 
 @Component({
   selector: 'app-vendor',
   templateUrl: './vendor.component.html',
   styleUrl: './vendor.component.css'
 })
-export class VendorComponent {
-  public vendorlist: [] = [];
-  constructor(private vendorService: VendorService) {
+export class VendorComponent implements OnInit {
+
+  public vendors: any[] = [];
+  public selectedVendorId: string | undefined;
+
+  constructor(private vendorService: VendorService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) {
   }
 
   ngOnInit(): void {
@@ -16,14 +28,38 @@ export class VendorComponent {
   }
 
   getAll() {
-    //this.branchService.getAll().subscribe({
-    //  next: (res) => {
-    //    this.branches = res.body;
-    //    console.log(res);
-    //  },
-    //  error: (err) => {
-    //    console.log(err);
-    //  }
-    //});
+    this.vendorService.getAll().subscribe({
+      next: (res) => {
+        this.vendors = res.body;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  onEdit(vendor: any) {
+    this.router.navigate(['./add-edit-vendor', vendor.id], { relativeTo: this.route });
+  }
+
+  onDelete(vendor: any) {
+    this.confirmationService.confirm({
+      message: `Are you sure to delete this <b>${vendor.name}</b>`,
+      accept: () => {
+        this.vendorService.delete(vendor.id)
+          .subscribe({
+            next: (res) => {
+              if (res.status === 200) {
+                this.getAll();
+                this.messageService.add({ key: 'toastKey1', severity: 'success', summary: `Vendor ${vendor.name} has been deleted.`, detail: '' });
+              }
+            },
+            error: (err) => {
+              this.messageService.add({ key: 'toastKey1', severity: 'error', summary: 'Failed to delete vendor.', detail: '' });
+            }
+          });
+      }
+    });
   }
 }
+    
