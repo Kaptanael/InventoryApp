76c8e9c2bd5a594896ab6576cba0fc0f8eb447fe
Md. Atikur Zaman <arzaman@ael-bd.com>
2024-02-01 16:00:36 +0600

    change service

--- InventoryApp.Client/src/app/_services/vendor.service.ts
+++ InventoryApp.Client/src/app/_services/vendor.service.ts
@@ -12,29 +12,29 @@ export class VendorService {
   }
 
   getAll(): Observable<HttpResponse<any>> {
-    return this.httpClient.get(`${resourceServerUrl}/api/v1/branch/getAll`, { observe: 'response' });
+    return this.httpClient.get(`${resourceServerUrl}/api/v1/Vendor/getAll`, { observe: 'response' });
   }
 
   getById(id: string): Observable<HttpResponse<any>> {
-    return this.httpClient.get(`${resourceServerUrl}/api/v1/branch/getById/${id}`, { observe: 'response' });
+    return this.httpClient.get(`${resourceServerUrl}/api/v1/Vendor/getById/${id}`, { observe: 'response' });
   }
 
   create(model: any): Observable<HttpResponse<any>> {
-    return this.httpClient.post(`${resourceServerUrl}/api/v1/branch/create`, model, {
+    return this.httpClient.post(`${resourceServerUrl}/api/v1/Vendor/create`, model, {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
     });
   }
 
   update(id: string, model: any): Observable<HttpResponse<any>> {
-    return this.httpClient.put(`${resourceServerUrl}/api/v1/branch/update/${id}`, model, {
+    return this.httpClient.put(`${resourceServerUrl}/api/v1/Vendor/update/${id}`, model, {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
     });
   }
 
   delete(id: string): Observable<HttpResponse<any>> {
-    return this.httpClient.delete(`${resourceServerUrl}/api/v1/branch/delete/${id}`, {
+    return this.httpClient.delete(`${resourceServerUrl}/api/v1/Vendor/delete/${id}`, {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
     });
